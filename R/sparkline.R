826fdfb9d4ce3a50ce0d31296d9de94265aa47b9
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2014-07-28 19:13:07 -0400

    pass width and height as options

--- R/sparkline.R
+++ R/sparkline.R
@@ -1,8 +1,9 @@
 #' @export
 #' @import htmltools
 #' @import htmlwidgets
-sparkline <- function(values, ..., width = 20, height = 10){
-  params = list(values = paste(values, collapse = ','), options = list(...), 
+sparkline <- function(values, ..., width = 60, height = 20){
+  params = list(values = paste(values, collapse = ','), 
+    options = list(..., height = height, width = width), 
     width = width , height = height
   )
   params = Filter(Negate(is.null), params)
