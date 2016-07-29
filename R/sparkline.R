59af8edcc6e3c72a26f6c5d58b79a738e121e435
timelyportfolio <kent.russell@timelyportfolio.com>
2016-07-29 17:37:38 -0500

    add roxygen template for shiny functions

--- R/sparkline.R
+++ R/sparkline.R
@@ -20,16 +20,32 @@ sparkline <- function(values, ..., width = 60, height = 20,
   )
 }
 
-#' @export
 sparkline_html <- function(id, style, class, ...){
   tags$span(id = id, class = class)
 }
 
+#' Shiny bindings for sparkline
+#'
+#' Output and render functions for using sparkline within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a sparkline
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name sparkline-shiny
+#'
 #' @export
 sparklineOutput <- function(outputId, width = "60px", height = "20px") {
   htmlwidgets::shinyWidgetOutput(outputId, "sparkline", width, height)
 }
 
+#' @rdname sparkline-shiny
 #' @export
 renderSparkline <- function(expr, env = parent.frame(), quoted = FALSE) {
   if (!quoted) { expr <- substitute(expr) } # force quoted
