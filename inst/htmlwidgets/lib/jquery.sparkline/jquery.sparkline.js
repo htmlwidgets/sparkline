c9a05b636e2143e7160b50f170e3e42ccab8b923
timelyportfolio <kent.russell@timelyportfolio.com>
2016-08-10 18:00:11 -0500

    update jquery.sparkline for fixed discrete

--- inst/htmlwidgets/lib/jquery.sparkline/jquery.sparkline.js
+++ inst/htmlwidgets/lib/jquery.sparkline/jquery.sparkline.js
@@ -2324,12 +2324,17 @@
         init: function (el, values, options, width, height) {
             discrete._super.init.call(this, el, values, options, width, height);
 
+            this.initTarget();
+
             this.regionShapes = {};
             this.values = values = $.map(values, Number);
             this.min = Math.min.apply(Math, values);
             this.max = Math.max.apply(Math, values);
             this.range = this.max - this.min;
-            this.width = width = options.get('width') === 'auto' ? values.length * 2 : this.width;
+            width = options.get('width') === 'auto' ? values.length * 2 * this.target.devicePixelRatio : this.width;
+            //adjust width for pixel ratio
+            width = width * this.target.devicePixelRatio;
+            this.width = width;
             this.interval = Math.floor(width / values.length);
             this.itemWidth = width / values.length;
             if (options.get('chartRangeMin') !== undefined && (options.get('chartRangeClip') || options.get('chartRangeMin') < this.min)) {
@@ -2338,14 +2343,14 @@
             if (options.get('chartRangeMax') !== undefined && (options.get('chartRangeClip') || options.get('chartRangeMax') > this.max)) {
                 this.max = options.get('chartRangeMax');
             }
-            this.initTarget();
+
             if (this.target) {
                 this.lineHeight = options.get('lineHeight') === 'auto' ? Math.round(this.canvasHeight * 0.3) : options.get('lineHeight');
             }
         },
 
         getRegion: function (el, x, y) {
-            return Math.floor(x * window.devicePixelRatio / this.itemWidth);
+            return Math.floor(x * this.target.ratio / this.itemWidth);
         },
 
         getCurrentRegionFields: function () {
